589b869bbbc2cb1199ca2f2590572ff0d9290e34
Vishnu Vardhan Reddy Mannuru <vishnuvardhanmannuru@gmail.com>
2026-04-26 12:11:20 +0530

    Updated home page

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from . import models
-# Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
+    list_display = ('title','category','author','status','is_featured',)
+    search_fields = ('title','category__category_name','status',)
+    list_editable = ('is_featured',)
+
 admin.site.register(models.Category)
+admin.site.register(models.Blog,BlogAdmin)
