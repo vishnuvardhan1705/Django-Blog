6c1f07701a7e2cc81ee485bcf1b32f7d7f6c6010
Vishnu Vardhan Reddy Mannuru <vishnuvardhanmannuru@gmail.com>
2026-04-27 11:15:38 +0530

    configured contect processors and post by category page

--- blog_main/views.py
+++ blog_main/views.py
@@ -2,11 +2,9 @@ from django.shortcuts import render
 from blogs.models import Category,Blog
 
 def home(request):
-    categories = Category.objects.all()
     featured_posts = Blog.objects.filter(is_featured=True,status='Published').order_by('updated_at')
     posts = Blog.objects.filter(is_featured=False,status='Published')
     context = {
-        'categories' : categories,
         'featured_posts' : featured_posts,
         'posts' :  posts,
     }
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import Blog,Category
 
-# Create your views here.
+def post_by_category(request,category_id):
+    posts = Blog.objects.filter(status='Published',category=category_id)
+    category = get_object_or_404(Category,pk=category_id)
+    '''try :
+        category = Category.objects.get(pk=category_id)
+    except :
+        return redirect('home')'''
+    context = {
+        'posts' : posts,
+        'category':category,        
+    }
+    print(posts)
+    return render(request, 'post_by_category.html',context)
