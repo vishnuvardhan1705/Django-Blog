27020531776d6146e196e3bbaeac0df3476bfc6b
Vishnu Vardhan Reddy Mannuru <vishnuvardhanmannuru@gmail.com>
2026-04-28 19:19:40 +0530

    Registration and login Functionality

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blogs',
     'social',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL ='/media/'
 MEDIA_ROOT = BASE_DIR/'media'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
