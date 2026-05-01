8e08d3e6ca1ef03d7bd9a72f735a7522d3e47b33
Vishnu Vardhan Reddy Mannuru <vishnuvardhanmannuru@gmail.com>
2026-05-01 11:47:09 +0530

    Posts crud operations/Functions

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -7,3 +7,8 @@ class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = '__all__'
+        
+class BlogPostForm(forms.ModelForm):
+    class Meta:
+        model = Blog
+        fields = ('title','category','featured_image','short_description','blog_body','status','is_featured')
--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,8 +3,16 @@ from . import views
 
 urlpatterns = [
     path('',views.dashboard,name='dashboard'),
+    
+    # Categories
     path('categories/',views.categories,name='categories'),
     path('categories/add/',views.add_category,name='add_category'),
     path('categories/edit/<int:pk>/',views.edit_category,name='edit_category'),
     path('categories/delete/<int:pk>/',views.delete_category,name='delete_category'),
+    
+    #Posts
+    path('posts/',views.posts,name='posts'),
+    path('posts/add/',views.add_post,name='add_post'),
+    path('posts/edit/<int:pk>/',views.edit_post,name='edit_post'),
+    path('postss/delete/<int:pk>/',views.delete_post,name='delete_post'),
 ]
