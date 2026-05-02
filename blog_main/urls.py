4b576a68a9c36cd22a7c1fa140bd2ca082567ec0
Vishnu Vardhan Reddy Mannuru <vishnuvardhanmannuru@gmail.com>
2026-05-02 10:34:24 +0530

    Search url

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -27,8 +27,7 @@ urlpatterns = [
     path('category/',include('blogs.urls')),
     path('blogs/<slug:slug>/',BlogViews.blogs,name='blogs'),
     
-    path('blogs/search/',BlogViews.search,name='search'),
-    
+    path('search/',BlogViews.search,name='search'),
     path('registration/',views.register,name='register'),
     path('login/',views.login,name='login'),
     path('logout/',views.logout,name='logout'),
